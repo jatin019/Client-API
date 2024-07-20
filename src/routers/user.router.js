1c22d3cd7fb5623c8d29bd2bd5cb36494f0d1efa
Jatin Singh <js.jatinsingh19@gmail.com>
2024-07-20 22:51:03 +0530

    password reset request endpoint created, random pin created and stored in new collection

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,37 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { insertUser, getUserByEmail } = require('../model/user/User.model');
-const { hashPassword, comparePassword } = require('../helpers/bcrypt.helper');
-const { createAccessJWT, createRefreshJWT } = require('../helpers/jwt.helper');
-const { userAuth } = require('../middlewares/auth.middleware');
+const {
+  insertUser,
+  getUserByEmail,
+  getUserById,
+} = require("../model/user/User.model");
+const { hashPassword, comparePassword } = require("../helpers/bcrypt.helper");
+const { createAccessJWT, createRefreshJWT } = require("../helpers/jwt.helper");
+const { userAuth } = require("../middlewares/auth.middleware");
+const {setPasswordResetPin} = require("../model/resetPin/RestPin.model");
 
-router.all('/', (req, res, next) => {
+router.all("/", (req, res, next) => {
   next();
 });
 
 // Get user profile router
-router.get('/', userAuth, (req, res) => {
+router.get("/", userAuth, (req, res) => {
   // The user object is now available in req.user
-  const { _id, name, company, address, phone, email, refreshJWT , password} = req.user;
+  const { _id, name, company, address, phone, email, refreshJWT, password } =
+    req.user;
 
   res.json({
     user: {
       refreshJWT: {
         token: refreshJWT.token,
-        addedAt: refreshJWT.addedAt },
+        addedAt: refreshJWT.addedAt,
+      },
       _id,
       name,
       company,
       address,
       phone,
       email,
-      password
-    }
+      password,
+    },
   });
 });
 // Create new user router
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { name, company, address, phone, email, password } = req.body;
 
   try {
@@ -50,47 +57,87 @@ router.post('/', async (req, res) => {
     const result = await insertUser(newUserObj);
     console.log(result);
 
-    res.json({ message: 'New user created', result });
+    res.json({ message: "New user created", result });
   } catch (error) {
     console.log(error);
-    res.json({ message: 'error', message: error.message });
+    res.json({ message: "error", message: error.message });
   }
 });
 
 // User sign in router
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   console.log(req.body);
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.json({ status: 'error', message: 'Invalid form submission!' });
+    return res.json({ status: "error", message: "Invalid form submission!" });
   }
 
   try {
     const user = await getUserByEmail(email);
 
     if (!user) {
-      console.log('Invalid credentials');
-      return res.json({ status: 'error', message: 'Invalid credentials!' });
+      console.log("Invalid credentials");
+      return res.json({ status: "error", message: "Invalid credentials!" });
     }
 
     // Compare the passwords
     const isPasswordMatch = await comparePassword(password, user.password);
-    console.log('Password match result:', isPasswordMatch);
+    console.log("Password match result:", isPasswordMatch);
 
     if (!isPasswordMatch) {
-      console.log('Invalid credentials');
-      return res.json({ status: 'error', message: 'Invalid credentials!' });
+      console.log("Invalid credentials");
+      return res.json({ status: "error", message: "Invalid credentials!" });
     }
 
-    const accessJWT = await createAccessJWT(user.email, `${user._id}`)
+    const accessJWT = await createAccessJWT(user.email, `${user._id}`);
     const refreshJWT = await createRefreshJWT(user.email, `${user._id}`);
 
-    res.json({ status: 'success', message: 'Login successfully!', accessJWT, refreshJWT });
+    res.json({
+      status: "success",
+      message: "Login successfully!",
+      accessJWT,
+      refreshJWT,
+    });
   } catch (error) {
-    console.log('Error during login:', error);
-    res.json({ status: 'error', message: error.message });
+    console.log("Error during login:", error);
+    res.json({ status: "error", message: error.message });
   }
+});
+
+// A. Create and send password reset pin number
+//  1. receive email
+//  2. check if user exist for the email
+//  3. create unique 6 digit pin
+//  4. save pin and email in database
+//  5. email the pin
+
+// B. Update password in database
+//  1. receive email, pin and new password
+//  2. validate pin
+//  3. encrypt new password
+//  4. update password in database
+//  5. send email notification
+
+// C. Server side form validation
+//  1. create middleware to validate from data
+
+router.post("/reset-password", async (req, res) => {
+  const { email } = req.body;
+
+  const user = await getUserByEmail(email);
+
+  if (user && user._id) {
+
+    // create //2. check if user exist for the email
+    const setPin = await setPasswordResetPin(email)
+    res.json(setPin)
+  }
+  // res.json({
+  //   status: "error",
+  //   message:
+  //     "If the email is exist in our database, the password reset pin will be sent shortly.",
+  // });
 });
 
 module.exports = router;
